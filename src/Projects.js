d8f131513bb9aef6e853133b8d68515853d1151a
Andrew Hong <andrewhong@myyahoo.com>
2022-11-04 22:08:49 -0400

    Change text values

--- src/Projects.js
+++ src/Projects.js
@@ -14,15 +14,15 @@ export default function Projects() {
             <div className="project">
                 <img src={scarletnavImg} alt="scarletnav"/>
                 <div className="info">
-                    <h2>Scarlet Navigator</h2>
-                    <p>A course planner for Rutgers University</p>
+                    <h2>RUSocial</h2>
+                    <p>Discord bot that keeps track of getINVOLVED events</p>
                 </div>
             </div>
             <div className="project">
                 <img src={scarletnavImg} alt="scarletnav"/>
                 <div className="info">
-                    <h2>Scarlet Navigator</h2>
-                    <p>A course planner for Rutgers University</p>
+                    <h2>Weighted Calculator</h2>
+                    <p>Plan your course grade ahead of time</p>
                 </div>
             </div>
         </div>
